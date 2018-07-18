b87759142d3bbb4bca014a8023415065eb7d0075
Sebastian Kopec <coder@soften.pl>
2018-07-18 10:57:13 +0200

    Source files

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import GamePuzzleSwap from './components/GamePuzzleSwap.vue';
+
+new Vue({
+    el: '#app',
+    components: {
+        'game-puzzle-swap': GamePuzzleSwap,
+    }
+});
